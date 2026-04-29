5a771f5106c8328ade23a9ba06a09134ec7f0a35
Ngha-Boris <borisngha6@gmail.com>
2026-04-29 11:11:15 +0100

    T-CI-001: complete implementation

--- fib/src/main.rs
+++ fib/src/main.rs
@@ -1,9 +1,9 @@
+use fib::fibonacci;
 use std::io;
 
 fn main() {
     println!("Hey there, welcome 🥳😃");
     loop {
-
         println!("1. Check the Fibonacci number at a particular position");
         println!("2. Check the Fibonacci numbers of integer values of the range 1..23");
         println!("3. Exit the program");
@@ -13,17 +13,14 @@ fn main() {
         io::stdin()
             .read_line(&mut number)
             .expect("Failed to read line");
-        
-        let number: u64 = match number.trim().parse() {
 
+        let number: u64 = match number.trim().parse() {
             Ok(number) => number,
-            Err(_) => { 
+            Err(_) => {
                 println!("Please enter either 1, 2, or 3");
                 continue;
             }
-            
         };
-
 
         match number {
             1 => {
@@ -36,31 +33,23 @@ fn main() {
                     Ok(int) => int,
                     Err(_) => continue,
                 };
-                println!("Fibonacci({}) --> {}", int, fibonacci(int as u64));
+                println!("Fibonacci({}) --> {}", int, fibonacci(int));
             }
             2 => {
                 println!("Hello there! Let's generate some nth Fibonacci numbers");
                 for num in 1..23 {
-                    println!("The value at position ({}) --> {}", num, fibonacci(num as u64));
+                    println!(
+                        "The value at position ({}) --> {}",
+                        num,
+                        fibonacci(num as u64)
+                    );
                 }
             }
-            3 => {  
+            3 => {
                 println!("Thank you for using our program 👋👋");
-                        break;
+                break;
             }
             _ => continue,
         }
-
-
-    }
-}
-
-fn fibonacci(n: u64) -> u64 {
-    if n == 0 {
-        return 0
-    } else if n == 1 {
-        return 1
-    } else {
-        fibonacci(n - 1) + fibonacci(n - 2)
     }
 }
